cf1874e86f7a456d075621ffb9849a9a1a7c82c3
someguyA123 <robots@ubiquibit.com>
2018-04-29 22:31:42 -0700

    aws is online

---
+++ delete-jumpbox.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+bosh delete-env \
+  ${BBL_STATE_DIR}/jumpbox-deployment/jumpbox.yml \
+  --state  ${BBL_STATE_DIR}/vars/jumpbox-state.json \
+  --vars-store  ${BBL_STATE_DIR}/vars/jumpbox-vars-store.yml \
+  --vars-file  ${BBL_STATE_DIR}/vars/jumpbox-vars-file.yml \
+  -o  ${BBL_STATE_DIR}/jumpbox-deployment/aws/cpi.yml \
+  -v  access_key_id="${BBL_AWS_ACCESS_KEY_ID}" \
+  -v  secret_access_key="${BBL_AWS_SECRET_ACCESS_KEY}" 
